b21f90292686d23fc0f82005cf0f2d624b6db2bd
Simon Funke <simon.funke@gmail.com>
2013-09-23 16:17:36 +0100

    be clear that Functional.gradient returns the Riez representation of the functinal derivative

--- moola/problem/functional.py
+++ moola/problem/functional.py
@@ -5,7 +5,10 @@ class Functional(object):
         raise NotImplementedError, "Functional.__eval__ is not implemented." 
 
     def gradient(self, x):
-        ''' Evaluates the gradient of the object functional at x. '''
+        ''' Evaluates the gradient of the object functional at x. 
+            The returned gradient is the Riez representation of the 
+            functional derivative at x, i.e. is a vector in the same space
+            as x. '''
         raise NotImplementedError, "Functional.gradient is not implemented." 
 
 class ObjectiveFunctional(Functional):

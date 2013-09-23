431d89eeaa15900b900edf93fe20c4bdc6525ea0
Simon Funke <simon.funke@gmail.com>
2013-09-23 19:47:24 +0100

    Add a cubic test example

---
+++ tests/cubic/cubic.py
@@ -0,0 +1,33 @@
+'''
+  Solves min_x 1/3 * (x[0]**3 + x[1]**3 + ... +x[n-1]**3),
+     x in R^n
+'''
+from moola import *
+import numpy as np
+from numpy.random import random
+
+class MyFunctional(ObjectiveFunctional):
+    def __call__(self, x):
+        arr = x.data
+        return 1/3.*sum(arr**3)
+
+    def gradient(self, x):
+        arr = x.data
+        return NumpyVector(arr**2)
+
+init_control = NumpyVector(random(5))
+
+obj = MyFunctional()
+prob = Problem(obj, init_control)
+
+
+options = {}
+options["gtol"] = 1e-20
+
+solver = SteepestDescent(tol=1e-200, options=options)
+print solver
+sol = solver.solve(prob)
+print sol
+
+assert sol["Optimizer"].norm("L2") < 1e-9
+assert sol["Number of iterations"] < 50

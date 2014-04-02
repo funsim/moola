61bb5a6e8acd20f67dd638b5e52cabfc23f952aa
Magne Nordaas <magneano@simula.no>
2014-04-02 19:13:46 +0200

    Fixed Fletcher-Reeves, also moved display function to OptimisationAlgorithm.

--- moola/algorithms/optimisation_algorithm.py
+++ moola/algorithms/optimisation_algorithm.py
@@ -13,7 +13,62 @@ class OptimisationAlgorithm(object):
     def __str__(self):
         ''' Prints out a description of the algorithm settings. '''
         raise NotImplementedError, 'OptimisationAlgorithm.__str__ is not implemented'
+    
+    def perform_line_search(self, xk, dk):
+        '''
+        Performs a line search for a new minimum.
+        xk is the current control point
+        pk is the direction of the search
 
+        This function should return a triple, consisting of:
+        The computed stepsize ak
+        The objective evaluated at xk +ak * dk
+        The derivative at xk +ak * dk, if computed
+        '''
+        p_last = None
+        djs = None
+        
+        def phi(alpha):
+            tmpx = xk.__class__(xk)
+            tmpx.axpy(alpha, pk)
+            return self.problem.obj(tmpx)
+
+        def phi_dphi(alpha):
+            tmpx = xk.__class__(xk)
+            tmpx.axpy(alpha, pk)
+            p = p_last = phi(alpha) 
+            djs = self.problem.obj.derivative(tmpx)(pk)
+            return p, djs
+
+        ak, Jnew, DJnew = self.ls.search(phi, phi_dphi)
+        return float(ak), Jk, DJk # TODO: numpy.float64 does not play nice with moola types
+
+    def check_convergence(self, it, J, oldJ, g):
+        s = 0
+        if it >= self.maxiter:
+            s = 1
+        if J != None and oldJ != None and oldJ-J < self.tol != None:
+            s = 2
+        if g!= None and g.norm() < self.gtol != None:
+            s = 3
+        self.status = s
+        return self.status
+
+    def display(self,it, J, oldJ, grad):
+        deltaJ = oldJ -J if (oldJ is not None and J is not None) else None
+        msg = 'Iteration {0:d}' \
+            + ('\tJ = {1:e}' if J is not None else '')\
+            + ('\t|dJ| = {2:e}' if grad is not None else '')\
+            + ('\tdeltaJ = {3:e}' if deltaJ is not None else '') 
+        if self.status == 0 and self.disp > 1:
+            print(msg.format(it, J, grad.norm(), deltaJ))
+        if self.status != 0 and self.disp > 0:
+            reasons = {1: '\nMaximum number of iterations reached.\n',
+                       2: '\nTolerance reached: |delta j| < tol.\n',
+                       3: '\nTolerance reached: |dJ| < gtol.\n'}
+            print(reasons[self.status] +  msg.format(it, J, grad.norm(), deltaJ))
+            
+    
     def solve(self, problem, m):
         ''' Solves the optimisation problem. 
             Arguments:
@@ -36,3 +91,5 @@ def get_line_search_method(line_search, line_search_options):
         raise ValueError, "Unknown line search specified. Valid values are 'armijo', 'strong_wolfe' and 'fixed'."
 
     return ls
+
+
--- tests/rosenbrock/rosenbrock.py
+++ tests/rosenbrock/rosenbrock.py
@@ -14,7 +14,7 @@ class MyFunctional(Functional):
 
     def derivative(self, val):
         x, y = val.data
-        print "current x =", x, y 
+        #print "current x =", x, y 
 
         dx = - 2*(1 - x) + 100*2*(y - x**2) * (-2*x)
         dy = 100 * 2 * (y - x**2)
@@ -29,20 +29,26 @@ prob = Problem(obj)
 
 x_opt = (1, 1)
 f_opt = 0
-options = {'gtol': None, 'maxiter': 1000}
+options = {'disp':1, 'tol': None, 'gtol': None, 'maxiter': 100, 'mem_lim': 1}
 
 # Solve the problem with the steepest descent method
-#solver = SteepestDescent(tol=None, options=options)
-#sol = solver.solve(prob, x_init)
+solver = SteepestDescent(tol=None, options=options)
+sol = solver.solve(prob, x_init.copy())
 #assert max(abs(sol["Optimizer"].data + x_opt)) < f_opt + 1e-9
 #assert sol["Number of iterations"] < 50
+print sol['Optimizer'].data
+print '\n\n\n'
 
 # Solve the problem with the Fletcher-Reeves method
-#solver = FletcherReeves(options=options)
-#sol = solver.solve(prob, x_init)
+solver = FletcherReeves(options=options)
+sol = solver.solve(prob, x_init.copy())
 #assert max(abs(sol["Optimizer"].data + x_opt)) < f_opt + 1e-9
 #assert sol["Number of iterations"] < 50
+print sol['Optimizer'].data
+print '\n\n\n'
 
 # Solve the problem with the BFGS method
 solver = BFGS(options=options)
-sol = solver.solve(prob, x_init)
+sol = solver.solve(prob, x_init.copy())
+print sol['Optimizer'].data
+print '\n\n\n'

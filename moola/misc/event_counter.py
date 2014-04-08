6d5069af1b8f8c11c77114cfc9bb7fc4f4ffab3d
Simon Funke <simon.funke@gmail.com>
2014-04-08 17:37:30 +0200

    Add missing file

---
+++ moola/misc/event_counter.py
@@ -0,0 +1,23 @@
+class EventCounter(dict):
+    ''' A simple class for counting events. '''
+
+    def increment(self, event_name):
+        try: 
+            self[event_name] += 1
+        except KeyError:
+            self[event_name] = 1
+
+    def __str__(self):
+        print ""
+        print "="*20 + "Moola Events" + "="*20
+        print ""
+        s = []
+
+        row_format ="{:>25}" * 2
+        for k, v in self.iteritems():
+            s.append(row_format.format(k, v))
+
+        return "\n".join(s)
+
+
+events = EventCounter()

445953602ef41b08dd83b1eee775a33a04aa89bf
Simon Funke <simon.funke@gmail.com>
2014-04-11 14:10:14 +0200

    Fix issues at the corners with much help from Andre Massing

--- tests/cauchy/cauchy.py
+++ tests/cauchy/cauchy.py
@@ -14,20 +14,12 @@ except ImportError:
     info_blue("moola bindings unavailable, skipping test")
     sys.exit(0)
 
-res = 32
+res = 64
 mesh = UnitSquareMesh(res, res)
 
 dolfin.set_log_level(ERROR)
 parameters['std_out_all_processes'] = False
 x = triangle.x
-
-class Left(SubDomain):
-    def inside(self, x, on_boundary):
-        return near(x[0], 0.0)
-
-class Right(SubDomain):
-    def inside(self, x, on_boundary):
-        return near(x[0], 1.0)
 
 class Bottom(SubDomain):
     def inside(self, x, on_boundary):
@@ -37,68 +29,56 @@ class Top(SubDomain):
     def inside(self, x, on_boundary):
         return near(x[1], 1.0)
 
-left = Left()    
-right = Right()    
 bottom = Bottom()
 top = Top()
 boundaries = FacetFunction("size_t", mesh)
 boundaries.set_all(0)
-
-left.mark(boundaries, 4)
-top.mark(boundaries, 3)
-right.mark(boundaries, 2)
 bottom.mark(boundaries, 1)
+top.mark(boundaries, 3)
 ds = Measure("ds")[boundaries]
 
 V = FunctionSpace(mesh, "CG", 1)
+W = FunctionSpace(mesh, "DG", 1)
 u = Function(V, name='State')
-g = Function(V, name='Control')
+g = Function(W, name='Control')
 v = TestFunction(V)
 w = TrialFunction(V)
-#g.interpolate(project(Expression("cos(x[0]*pi)"), V))
-#plot(g, title="g")
-
 n = FacetNormal(mesh)
 h = CellSize(mesh)
 
 def solve_pde(u, g):
-    gamma = Constant(1000)
-    F = inner(grad(w), grad(v))*dx - inner(grad(w), n)*v*ds(3) + gamma/h*(g-w)*v*ds(3)
+    gamma = Constant(10)
+    F = inner(grad(w), grad(v))*dx - inner(grad(w), n)*v*ds(3) - inner(grad(v), n)*(w-g)*ds(3) + gamma/h*(w-g)*v*ds(3)
     solve(lhs(F) == rhs(F), u)
 
 # Run the forward model once to create the annotation
 solve_pde(u, g)
-#plot(u, title="u", interactive=True)
 
 # ========= Example settings ===============
 #d = sin(pi*x[0])            # Data for example 3
 d = cos(pi*x[0]) / cosh(pi) # Data for example 4
 alpha = Constant(0)         # Regularisation multiplier
 delta = Constant(0)         # Perturbation multiplier
-
 d += delta*cos(5*pi*x[0])
 
-print "Initial J = ", assemble(inner(u-d, u-d)*ds(1))
 J = Functional((inner(u-d, u-d))*ds(1) + alpha*g**2*ds(3))
-rf = ReducedFunctional(J, SteadyParameter(g))
+m = SteadyParameter(g)
+
+rf = ReducedFunctional(J, m)
 
 problem = rf.moola_problem()
-solver = moola.BFGS(tol=None, options={'gtol': 1e-7, 'maxiter': 20, 'mem_lim': 20})
-#solver = moola.NewtonCG(tol=1e-200, options={'gtol': 1e-10, 'maxiter': 20, 'ncg_reltol':1e-20, 'ncg_hesstol': "default"})
+#solver = moola.BFGS(tol=None, options={'gtol': 1e-11, 'maxiter': 20, 'mem_lim': 20})
+solver = moola.NewtonCG(tol=1e-200, options={'gtol': 1e-10, 'maxiter': 20, 'ncg_reltol':1e-20, 'ncg_hesstol': "default"})
 g_moola = moola.DolfinPrimalVector(g)
 sol = solver.solve(problem, g_moola)
 g_opt = sol['Optimizer'].data
 
-#g_opt = minimize(rf, method="Newton-CG", options={"xtol": 1e-100})
+#g_opt = minimize(rf, method="BFGS", tol=1e-09, options={"xtol": 1e-100})
 
 plot(d, title="d", mesh=mesh)
-#plot(g_opt, title="g*")
-
 solve_pde(u, g_opt)
 plot(u, title="u*")
 interactive()
-
-
 
 print "Final J = ", assemble(inner(u-d, u-d)*ds(1))
 print "||g* - g_analytic|| = ", assemble((g_opt - cos(pi*x[0]))**2*ds(3))**0.5

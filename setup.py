1e1cdc3e62d1428616932060aafb2453b790db22
Simon Funke <simon.funke@gmail.com>
2014-04-11 20:08:20 +0200

    Add a setup.py file for easy installation

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='moola',
+      version='0.1',
+      description='Moola optimisation package',
+      author='Simon Funke',
+      author_email='simon@simula.no',
+      url='https://github.com/funsim/moola',
+      packages=find_packages(),
+      classifiers=[
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Operating System :: OS Independent',
+            'Environment :: Console',
+            'Intended Audience :: Science/Research',
+            'Topic :: Scientific/Engineering :: Mathematics'
+      ]
+)

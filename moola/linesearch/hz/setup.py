a754ff5d6105939609fd2fa84cb5f796fcc81b90
Simon Funke <simon.funke@gmail.com>
2014-05-13 14:40:12 +0200

    Remove cython hager-zhang linesearch code

--- moola/linesearch/hz/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-from distutils.extension import Extension
-
-setup(
-    ext_modules = cythonize("hz_linesearch.pyx")
-)
--- moola/linesearch/hz/test.py
+++
@@ -1,9 +0,0 @@
-from hz_linesearch import HZLineSearch
-
-hz = HZLineSearch()
-
-hz.set_print_level(3)
-hz.print_parameters()
-
-print "Running search"
-hz.search()
